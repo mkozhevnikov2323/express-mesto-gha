c8d0f414f5d5df345025a2212ad4c2d77e32fabd
Maksim Kozhevnikov <mkozhevnikov23@gmail.com>
2022-06-15 01:53:00 +0400

    dev: create test user

--- routes/users.js
+++ routes/users.js
@@ -4,3 +4,5 @@ const { createUser, getUsers, getUserById } = require('../controllers/users');
 router.post('/users', createUser);
 router.get('/users', getUsers);
 router.get('/users/:id', getUserById);
+
+module.exports = router;
