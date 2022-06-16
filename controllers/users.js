055ad2fa1d432790d76b1bf916cebf19530be5af
Maksim Kozhevnikov <mkozhevnikov23@gmail.com>
2022-06-16 18:31:28 +0400

    dev: add user errors

--- controllers/users.js
+++ controllers/users.js
@@ -18,8 +18,14 @@ module.exports.getUsers = (req, res) => {
 
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.id)
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .then((user) => {
+      if (user === null) {
+        res.status(404).send({ message: 'Пользователь не найден' });
+        return;
+      }
+      res.send({ data: user });
+    })
+    .catch(() => res.status(400).send({ message: 'Некорректный параметр id' }));
 };
 
 module.exports.updateUserProfile = (req, res) => {
@@ -27,13 +33,15 @@ module.exports.updateUserProfile = (req, res) => {
 
   User.findByIdAndUpdate(req.params.id, { name, about })
     .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch(() => {
+      res.status(400).send({ message: 'Поле name и about должны содержать от 2 до 30 символов.' });
+    });
 };
 
 module.exports.updateUserAvatar = (req, res) => {
-  const { about } = req.body;
+  const { avatar } = req.body;
 
-  User.findByIdAndUpdate(req.params.id, { about })
+  User.findByIdAndUpdate(req.params.id, { avatar })
     .then((user) => res.send({ data: user }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
