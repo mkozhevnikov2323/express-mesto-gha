75c8bc420afddc09e979a15f3f578d42adc582e0
Maksim Kozhevnikov <mkozhevnikov23@gmail.com>
2022-06-26 18:29:04 +0400

    dev: fix path to controllers login

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const { createUser, login } = require('../controllers/users');
+const { createUser, login } = require('./controllers/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
