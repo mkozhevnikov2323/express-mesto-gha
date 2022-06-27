ea4cc8bcaef04db5fa6dfecf21959d871b250cf2
Maksim Kozhevnikov <mkozhevnikov23@gmail.com>
2022-06-27 21:43:21 +0400

    dev: add joi to update user

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-// const { celebrate, Joi } = require('celebrate');
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getUserInfo, getUsers, getUserById, updateUserProfile, updateUserAvatar,
@@ -7,8 +7,21 @@ const {
 
 router.get('/me', getUserInfo);
 router.get('/', getUsers);
-router.get('/:id', getUserById);
-router.patch('/me', updateUserProfile);
-router.patch('/me/avatar', updateUserAvatar);
+router.get('/:id', celebrate({
+  params: Joi.object().keys({
+    id: Joi.string().alphanum().length(24),
+  }),
+}), getUserById);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), updateUserProfile);
+router.patch('/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string(),
+  }),
+}), updateUserAvatar);
 
 module.exports = router;
