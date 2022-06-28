3341abd0017fee47436c5316f5fcea956e2d97bb
Maksim Kozhevnikov <mkozhevnikov23@gmail.com>
2022-06-29 01:35:08 +0400

    dev: fix g-flag on joi

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.post('/signup', celebrate({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     // eslint-disable-next-line no-useless-escape
-    avatar: Joi.string().pattern(/https?:\/\/(w{3}\.)?[\w\-\.\~\:\/\?\#\[\]\@\!\$\&\'\(\)\*\+\,\;\=]{2,}/gmi),
+    avatar: Joi.string().pattern(/https?:\/\/(w{3}\.)?[\w\-\.\~\:\/\?\#\[\]\@\!\$\&\'\(\)\*\+\,\;\=]{2,}/mi),
   }),
 }), createUser);
 
