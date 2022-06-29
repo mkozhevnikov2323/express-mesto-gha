43750452d18932d6a2fba37036ee3eb8ea4f6cc9
Maksim Kozhevnikov <mkozhevnikov23@gmail.com>
2022-06-29 23:28:43 +0400

    dev: fix some errors

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.use((err, req, res, next) => {
     .status(statusCode)
     .send({
       message: statusCode === 500
-        ? 'На сервере произошла ошибка'
+        ? ('На сервере произошла ошибка', message, err.code)
         : message,
     });
 });
