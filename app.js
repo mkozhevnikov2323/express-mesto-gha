74506c60ef989fbe07bf33be34c79186d47ff944
Maksim Kozhevnikov <mkozhevnikov23@gmail.com>
2022-06-30 21:05:47 +0400

    hotfix: ref appjs

--- app.js
+++ app.js
@@ -4,16 +4,17 @@ const bodyParser = require('body-parser');
 const { errors, celebrate, Joi } = require('celebrate');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
+const NotFoundError = require('./errors/not-found-err');
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -28,8 +29,7 @@ app.post('/signup', celebrate({
     password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    // eslint-disable-next-line no-useless-escape
-    avatar: Joi.string().pattern(/https?:\/\/(w{3}\.)?[\w\-\.\~\:\/\?\#\[\]\@\!\$\&\'\(\)\*\+\,\;\=]{2,}/mi),
+    avatar: Joi.string().pattern(/https?:\/\/(w{3}\.)?[\w\-.~:/?#[\]@!$&'()*+,;=]{2,}/mi),
   }),
 }), createUser);
 
@@ -37,8 +37,8 @@ app.use(auth);
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.patch('/*', (req, res) => {
-  res.status(404).send({ message: 'Страница не найдена.' });
+app.patch('/*', () => {
+  throw new NotFoundError('Страница не найдена!');
 });
 
 app.use(errors());
@@ -48,26 +48,19 @@ app.use((err, req, res, next) => {
 
   if (err.code === 11000) {
     res.status(409).send({ message: 'Пользователь с данным E-mail присутствует в базе.' });
-    return;
-  }
-  if (err.code === 13) {
-    res.status(401).send({ message: 'Неправильные почта или пароль!' });
-    return;
-  }
-  if (err.name === 'ValidationError') {
+  } else if (err.name === 'ValidationError') {
     res.status(400).send({ message: 'Некорректный email или длина пароля менее 8 символов.' });
-  }
-  if (err.name === 'CastError') {
+  } else if (err.name === 'CastError') {
     res.status(400).send({ message: 'Некорректный id.' });
+  } else {
+    res
+      .status(statusCode)
+      .send({
+        message: statusCode === 500
+          ? ('На сервере произошла ошибка')
+          : message,
+      });
   }
-
-  res
-    .status(statusCode)
-    .send({
-      message: statusCode === 500
-        ? ('На сервере произошла ошибка')
-        : message,
-    });
 });
 
 app.listen(PORT);
