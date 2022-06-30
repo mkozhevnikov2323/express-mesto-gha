faf7b1d053a6eec6b97503d7638fba17ff9bcb3c
Maksim Kozhevnikov <mkozhevnikov23@gmail.com>
2022-06-30 19:53:01 +0400

    hotfix: ref middleware auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,24 +1,20 @@
 const jwt = require('jsonwebtoken');
+const AuthorizationError = require('../errors/authorization-err');
 
-// eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    throw new AuthorizationError('Необходима авторизация!');
   }
 
   const token = authorization.replace('Bearer ', '');
-  let payload = '123';
+  let payload;
 
   try {
     payload = jwt.verify(token, '2e48302a6e4e6f4d364e51ef2d924411121f752eb4087571abe112de648773ff');
   } catch (err) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация!' });
+    throw new AuthorizationError('Необходима авторизация!');
   }
   req.user = payload;
   next();
